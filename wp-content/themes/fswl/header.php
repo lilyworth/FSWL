6884713263fc9ed65ca9b326243db9ec2d0b262c
lily <lilyworth@gmail.com>
2016-02-27 21:16:58 +0000

    Wordpress install & initial theme files

---
+++ wp-content/themes/fswl/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title><?php bloginfo('From Syria with Love'); ?> <?php wp_title(); ?></title> 
+<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
+
+<!-- disable iPhone inital scale -->
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen,projection" />
+
+<?php wp_head(); ?> 
+</head>
+<body>
+<div class="main">
+    <header>
+        <div class="logo">
+            <a href="<?php home_url(); ?>">From Syria with Love</a>
+        </div>
+    </header>
